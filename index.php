bb6d697dadc621b063a1b46e1d2a33c0fd5eb0c1
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2022-10-12 00:18:08 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,62 +1,53 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono&display=swap" rel="stylesheet">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
 </head>
-<style>
-    body {
-        font-family: 'Noto Sans Mono', monospace;   
-    }
-</style>
 <body>
-    <h3>Currency Converter</h3>
-    <form action="show.php" method="POST">
-        <div class="row">
-            <label for="change">From:</label>
-            <select id="change" name="change">
-            
-            <?php
-                $url = "http://10.0.15.20/lab8/restapis/currencyrate";    
-                $response = file_get_contents($url);
-                $result = json_decode($response);
-                
-                foreach ($result->rates as $key=>$value){
-                    if($key == "THB" || $key == "JPY" || $key == "CNY" || $key == "SGD" || $key == "USD"){
-                        echo "<option value='$value'>". $key ."</option>"; 
-                    }
-                }
-                
-            ?>
-            </select>
-            <input type="number" id="number" name="number" placeholder="Enter number" required />
-        </div>
-        <br>
-        <div class="row">
-            <label for="change2">To:</label>
-            <select id="change2" name="change2">
-        
-            <?php
-                $url = "http://10.0.15.20/lab8/restapis/currencyrate";    
-                $response = file_get_contents($url);
-                $result = json_decode($response);
-                
-                foreach ($result->rates as $key=>$value){
-                    if($key == "THB" || $key == "JPY" || $key == "CNY" || $key == "SGD" || $key == "USD"){
-                        echo "<option value='$value'>". $key ."</option>"; 
-                    }
-                }
-            ?>
-            </select>
-            <input type="text" id="prodname" name="prodname" placeholder=""/>
-        </div>
-        <input type="text" id="ans" name="name" placeholder="ans">
-        <button type="submit" name="submit">Convert</button>
-        
-    </form>
+   
+<form method="POST">
+<?php
+   // Connect to Database 
+   class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('customers.db');
+      }
+   }
+
+   // Open Database 
+   $db = new MyDB();
+   if(!$db) {
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully<br>";
+   }
+
+   $sql ="SELECT * from customers";
+   $ret = $db->query($sql);
+   $num = 0;
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+      echo $row['CustomerId'] . " ";
+      echo $row['FirstName'] ." ";
+      echo $row['LastName'] ." ";
+      echo $row['Company'] ."<br><br>";
+      $num = $row['CustomerId'];
+   }
+   echo "<button type='submit' name='submit'>del</button>";
+   if(isset($_POST["submit"])){
+      $sql = "DELETE from customers where CustomerId = $num";
+      $ret = $db->exec($sql);
+      if(!$ret){
+        echo $db->lastErrorMsg();
+      } else {
+         echo $db->changes(),"finish";
+         header('Refresh: 0');
+      }
+   }
+   $db->close();
+
+?>
+</form>
 </body>
 </html>
